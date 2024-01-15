5166581c885291660af36124251f784443d27a46
JM1705 <73152770+JM1705@users.noreply.github.com>
2024-01-15 21:02:47 +1100

    Initial commit - only moving a file works

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 use std::fs;
 
+// fn print_type_of<T>(_: &T) {
+//     println!("{}", std::any::type_name::<T>())
+// }
+
 fn main() {
+    movefile("file1.txt", "file2.txt")
+}
+
+fn movefile(start: &str, end: &str) {
     println!("TLP config switcher");
-    fs::rename("~/Desktop/test/folder1/file1", "~/Desktop/test/folder1/file2")?;
+
+    let dir = "./";
+
+    let mut dir_files = fs::read_dir(dir)
+      .expect("failed to read directory")
+        .map(|file| file.unwrap().file_name());
+
+    // for file in dir_files {
+    //     println!("{:?}",file);
+    // }
+
+    if dir_files.any(|file| file==start) {
+        fs::rename(start, end)
+            .expect("could not rename file");
+        println!("moved {start} to {end}")
+    } else {
+        println!("file1.txt could not be found")
+    }
 }
 
